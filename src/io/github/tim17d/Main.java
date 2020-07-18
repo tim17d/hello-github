9db37dcb0d9883989220fd5ecc171aa60b2cf334
tim17d <68468445+tim17d@users.noreply.github.com>
2020-07-18 23:50:42 +0300

    Initial commit

---
+++ src/io/github/tim17d/Main.java
@@ -0,0 +1,9 @@
+package io.github.tim17d;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String env = "GitHub ";
+        System.out.println("Hello " + env + '!');
+    }
+}
